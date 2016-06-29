f9b12ee5ce886c64b8de37c18c2bfbcc2b710817
Joe Bolla <joe.bolla@wpnc.onmicrosoft.com>
2016-06-29 23:03:16 +0100

    more changes for dbscripts

---
+++ DBScripts/24. Create column ConversionRate in tb_Currency.sql
@@ -0,0 +1,7 @@
+
+IF NOT EXISTS(SELECT * FROM sys.columns
+WHERE Name = N'ConversionRate' AND OBJECT_ID = OBJECT_ID(N'tb_Currency'))
+BEGIN
+	ALTER TABLE tb_Currency add ConversionRate DECIMAL(18,2) NOT NULL DEFAULT(1.0)
+	PRINT 'Your Column ConversionRate has been created in table tb_Currency'
+END 
--- bigbus.checkout.data/Repositories/Implementation/GenericDataRepository.cs
+++ bigbus.checkout.data/Repositories/Implementation/GenericDataRepository.cs
@@ -1,51 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 using bigbus.checkout.data.Repositories.Infrastructure;
 using  bigbus.checkout.data;
+using bigbus.checkout.data.Model;
 
 namespace bigbus.checkout.data.Repositories.Implementation
 {
     public class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
     {
-        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+
+        private void Log(string message)
         {
-            List<T> list;
             using (var context = new CheckoutDbContext())
             {
-                IQueryable<T> dbQuery = context.Set<T>();
-
-                //Apply eager loading
-                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
-                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
-
-                list = dbQuery
-                    .AsNoTracking()
-                    .ToList<T>();
+                context.Logs.Add(new Log
+                {
+                    Level = "1",
+                    Logger = "GenericDataRepository",
+                    Message = message
+                });
             }
-            return list;
+        }
+
+        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            try
+            {
+                List<T> list;
+                using (var context = new CheckoutDbContext())
+                {
+
+                    IQueryable<T> dbQuery = context.Set<T>();
+
+                    //Apply eager loading
+                    foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                        dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                    list = dbQuery
+                        .AsNoTracking()
+                        .ToList<T>();
+                }
+                return list;
+            }
+            catch (DbEntityValidationException e)
+            {
+                var sbTemp = new StringBuilder();
+
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    sbTemp.AppendLine(
+                        string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State));
+
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        sbTemp.AppendLine(string.Format("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                Log(sbTemp.ToString());
+                throw;
+            }
+
         }
 
         public virtual IList<T> GetList(Func<T, bool> where,
              params Expression<Func<T, object>>[] navigationProperties)
         {
-            List<T> list;
-            using (var context = new CheckoutDbContext())
+
+            try
             {
-                IQueryable<T> dbQuery = context.Set<T>();
+                List<T> list;
+                using (var context = new CheckoutDbContext())
+                {
+                    IQueryable<T> dbQuery = context.Set<T>();
 
-                //Apply eager loading
-                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
-                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+                    //Apply eager loading
+                    foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                        dbQuery = dbQuery.Include<T, object>(navigationProperty);
 
-                list = dbQuery
-                    .AsNoTracking()
-                    .Where(where)
-                    .ToList<T>();
+                    list = dbQuery
+                        .AsNoTracking()
+                        .Where(where)
+                        .ToList<T>();
+
+                }
+                return list;
             }
-            return list;
+            catch(DbEntityValidationException e)
+            {
+                var sbTemp = new StringBuilder();
+
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    sbTemp.AppendLine(string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State));
+
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        sbTemp.AppendLine(string.Format("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                Log(sbTemp.ToString());
+                throw;
+            }
         }
 
         public virtual T GetSingle(Func<T, bool> where,
